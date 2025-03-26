8bf30b027b2cf027167998121e1585ea9afaacee
deya19 <deyasaleh2@gmail.com>
2025-03-27 01:07:43 +0300

    mock_intreview_platform

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function authLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="auth-layout">{children}</div>
+  )
+}
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <AuthForm type="sign-in"/>
+  )
+}
