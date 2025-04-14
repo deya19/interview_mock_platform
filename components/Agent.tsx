e48ac8d9456ee1ed5c9d1b49c07d36d1f581c809
deya19 <deyasaleh2@gmail.com>
2025-04-14 23:06:40 +0300

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
@@ -77,10 +78,13 @@ export default function Agent({
   const handleGenerateFeedback = async (messages: SaveMessage[]) => {
     console.log("Generate feedback here.");
 
-    const { success, id } = {
-      success: true,
-      id: "feedback-id",
-    };
+    const { success, feedbackId: id } = await createFeedback(
+      {
+        interviewId: interviewId!,
+        userId: userId!,
+        transcript: messages
+      }
+    );
 
     if (success && id) {
       router.push(`/interview/${interviewId}/feedback`);
@@ -94,6 +98,7 @@ export default function Agent({
     if (callStatus === CallStatus.FINISHED) {
       if (type === "generate") {
         router.push("/");
+        // console.log("Generate feedback here.");
       } else {
         handleGenerateFeedback(messages);
       }
